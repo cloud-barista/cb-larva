efc2f888aaad729114e3d90e9d8869356a988af0
Yunkon (Alvin) Kim <hermitkim1@users.noreply.github.com>
2020-10-26 21:50:40 +0900

    Create 2.install-dependency.sh

---
+++ poc-cb-net/2.install-dependency.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Install dependency
+apt install -y graphviz autoconf automake bzip2 debhelper dh-autoreconf libssl-dev libtool openssl procps python-all python-qt4 python-twisted-conch python-zopeinterface
