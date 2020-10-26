ee8753ee8f8c02c851f22c03464d1f1db19a5aea
Yunkon (Alvin) Kim <hermitkim1@gmail.com>
2020-10-26 22:30:48 +0900

    Create 3.install-ovs-2.5.10.sh
    - This includes a bug fix of Open vSwitch 2.5.10 source code.

---
+++ poc-cb-net/3.install-ovs-2.5.10.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get Open vSwitch 2.5.10
+wget https://www.openvswitch.org/releases/openvswitch-2.5.10.tar.gz
+
+# Decompress
+tar -zxvf openvswitch-2.5.10.tar.gz
+
+# Enter to the directory
+cd openvswitch-2.5.10
+
+# copy dhparams.c for backup
+cp ./lib/dhparams.c ./lib/dhparams.c.backup
+sleep 1
+
+# Delete static for debug (in 3 function declaration in .h file, there are no static keywords)
+sed -i 's/static //g' ./lib/dhparams.c
+sleep 1
+
+# Build Open vSwitch 
+DEB_BUILD_OPTIONS='parallel=8 nocheck' fakeroot debian/rules binary
