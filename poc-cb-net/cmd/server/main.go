8e6371f2a7ba2724eeef9197e4c91760c703c08a
Yunkon (Alvin) Kim <hermitkim1@gmail.com>
2020-10-30 18:14:53 +0900

    Inquire and share the network interface information
    - Design and create data objects for the network interface information
    - Design and create CBNetworkAgent struct and its functions
    - Test the messsage delivery between agent and server

--- poc-cb-net/cmd/server/main.go
+++ poc-cb-net/cmd/server/main.go
@@ -1,46 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	//import the Paho Go MQTT library
+	dataobjects "github.com/cloud-barista/cb-larva/poc-cb-net/data-objects"
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 	"os"
 )
 
 //define a function for the default message handler
 var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
-	fmt.Printf("TOPIC: %s\n", msg.Topic())
-	fmt.Printf("MSG: %s\n", msg.Payload())
+	fmt.Printf("Recieved TOPIC : %s\n", msg.Topic())
+	//fmt.Printf("MSG: %s\n", msg.Payload())
+
+	if msg.Topic() == "cb-net/network-host-information" {
+
+		// Unmarshal the network interfaces
+		var temp2 []dataobjects.NetworkInterface
+
+		_ = json.Unmarshal([]byte(msg.Payload()), &temp2)
+		fmt.Println("Unmarshalled JSON")
+		fmt.Println(temp2)
+
+		prettyJSON, _ := json.MarshalIndent(temp2, "", "\t")
+		fmt.Println("Pretty JSON")
+		fmt.Println(string(prettyJSON))
+	}
 }
 
 func main() {
-	//create a ClientOptions struct setting the broker address, clientid, turn
-	//off trace output and set the default message handler
+	// Create a ClientOptions struct setting the broker address, clientid, turn
+	// off trace output and set the default message handler
 	opts := MQTT.NewClientOptions().AddBroker("tcp://mqtt.eclipse.org:1883")
 	opts.SetClientID("cb-net-server")
 	opts.SetDefaultPublishHandler(f)
 
-
-	//create and start a client using the above ClientOptions
+	// Create and start a client using the above ClientOptions
 	c := MQTT.NewClient(opts)
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
 
-	//subscribe to the topic /go-mqtt/sample and request messages to be delivered
-	//at a maximum qos of zero, wait for the receipt to confirm the subscription
+	// Subscribe to the topic /go-mqtt/sample and request messages to be delivered
+	// at a maximum qos of zero, wait for the receipt to confirm the subscription
 	if token := c.Subscribe("cb-net/network-host-information", 0, nil); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error())
 		os.Exit(1)
 	}
-
-	////Publish 5 messages to /go-mqtt/sample at qos 1 and wait for the receipt
-	////from the server after sending each message
-	//for i := 0; i < 5; i++ {
-	//	text := fmt.Sprintf("this is msg #%d!", i)
-	//	token := c.Publish("go-mqtt/sample", 0, false, text)
-	//	token.Wait()
-	//}
 
 	fmt.Println("Press the Enter Key to stop anytime")
 	fmt.Scanln()
@@ -50,7 +56,6 @@ func main() {
 		fmt.Println(token.Error())
 		os.Exit(1)
 	}
-
 
 	c.Disconnect(250)
 }
