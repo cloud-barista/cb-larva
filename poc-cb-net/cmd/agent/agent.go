878d835cb0b5ea8b8fa15e8a3e79e8e25d5b3286
Yunkon (Alvin) Kim <hermitkim1@gmail.com>
2021-01-05 17:23:38 +0900

    Enhance agent and server
    - Apply cb-log to agent and server
    - Change endpoint information of MQTTBroker as MQTTBroker tool changes
    - Remove hardcoded MQTTBroker endpoint information
    - Update go mod

--- poc-cb-net/cmd/agent/agent.go
+++ poc-cb-net/cmd/agent/agent.go
@@ -7,44 +7,56 @@ import (
 	"github.com/cloud-barista/cb-larva/poc-cb-net/internal"
 	"github.com/cloud-barista/cb-larva/poc-cb-net/internal/app"
 	dataobjects "github.com/cloud-barista/cb-larva/poc-cb-net/internal/data-objects"
+	cblog "github.com/cloud-barista/cb-log"
 	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"github.com/sirupsen/logrus"
 	"math/big"
 	"os"
+	"path/filepath"
 )
 
 var CBNet *internal.CBNetwork
 var channel chan bool
+var CBLogger *logrus.Logger
+
+func init() {
+	// cblog is a global variable.
+	configPath := filepath.Join("..", "..", "configs", "log_conf.yaml")
+	CBLogger = cblog.GetLoggerWithConfigPath("cb-network", configPath)
+}
 
 //define a function for the default message handler
 var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
-	fmt.Printf("TOPIC: %s\n", msg.Topic())
-	fmt.Printf("MSG: %s\n", msg.Payload())
+	CBLogger.Info("Start.........")
+	CBLogger.Debugf("Received TOPIC : %s\n", msg.Topic())
+	CBLogger.Debugf("MSG: %s\n", msg.Payload())
 
 	if msg.Topic() == "cb-net/networking-rule" {
 
 		var networkingRule dataobjects.NetworkingRule
 
-		err := json.Unmarshal([]byte(msg.Payload()), &networkingRule)
+		err := json.Unmarshal(msg.Payload(), &networkingRule)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println("Unmarshalled JSON")
-		fmt.Println(networkingRule)
+		CBLogger.Debug("Unmarshalled JSON")
+		CBLogger.Debug(networkingRule)
 
-		//prettyJSON, _ := json.MarshalIndent(networkingRule, "", "\t")
-		//fmt.Println("Pretty JSON")
-		//fmt.Println(string(prettyJSON))
+		prettyJSON, _ := json.MarshalIndent(networkingRule, "", "\t")
+		CBLogger.Debug("Pretty JSON")
+		CBLogger.Debug(string(prettyJSON))
 
+		CBLogger.Debug("Update the networking rule")
 		CBNet.SetNetworkingRule(networkingRule)
-		fmt.Println("1")
 		if !CBNet.IsRunning() {
 			CBNet.StartCBNetworking(channel)
 		}
-		fmt.Println("3")
+		CBLogger.Info("End.........")
 	}
 }
 
 func main() {
+	CBLogger.Info("Start.........")
 
 	var arg string
 	if len(os.Args) > 1 {
@@ -58,7 +70,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(n)
+	CBLogger.Debugf("Random number: %d\t", n)
 
 	// Create CBNetwork instance with port, which is tunneling port
 	CBNet = internal.NewCBNetwork("cbnet0", 20000)
@@ -66,7 +78,7 @@ func main() {
 	// Load a config of MQTTBroker
 	config, _ := dataobjects.LoadConfigMQTTBroker()
 	// Create a endpoint link of MQTTBroker
-	server := "tcp://" + config.MQTTBrokerIP + ":" + config.MQTTBrokerPort
+	server := "tcp://" + config.MQTTBrokerHost + ":" + config.MQTTBrokerPort
 
 	// Create a ClientOptions struct setting the broker address, clientid, turn
 	// off trace output and set the default message handler
@@ -83,14 +95,14 @@ func main() {
 	// Subscribe to the topic /go-mqtt/sample and request messages to be delivered
 	// at a maximum qos of zero, wait for the receipt to confirm the subscription
 	if token := c.Subscribe("cb-net/networking-rule", 0, nil); token.Wait() && token.Error() != nil {
-		fmt.Println(token.Error())
+		CBLogger.Error(token.Error())
 		os.Exit(1)
 	}
 
 	// Get the VM network information
 	temp := CBNet.GetVMNetworkInformation()
 	doc, _ := json.Marshal(temp)
-	fmt.Println(string(doc))
+	CBLogger.Debug(string(doc))
 
 	// Publish a message to /cb-net/vm-network-information at qos 1 and wait for the receipt
 	// from the server after sending each message
@@ -110,7 +122,7 @@ func main() {
 
 	//Unsubscribe from /cb-net/vm-network-information"
 	if token := c.Unsubscribe("cb-net/networking-rule"); token.Wait() && token.Error() != nil {
-		fmt.Println(token.Error())
+		CBLogger.Error(token.Error())
 		os.Exit(1)
 	}
 
