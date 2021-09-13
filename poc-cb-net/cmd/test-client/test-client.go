13baa447ad62c4eeebc0b6d958afec91e335c34c
Yunkon (Alvin) Kim <hermitkim1@gmail.com>
2021-09-13 18:57:31 +0900

    Lay the foundations for APIs
    - Create cloud_adaptive_network.proto
    - Create a Makefile to (Re)compile proto for Golang
    - Generate a stub for Golang by compiling the proto
    - Test server and client by test-server.go and test-client.go

---
+++ poc-cb-net/cmd/test-client/test-client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/cloud-barista/cb-larva/poc-cb-net/pkg/api/gen/go"
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	conn, err := grpc.Dial("localhost:8088", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("Cannot connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewCloudAdaptiveNetworkClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.CreateCLADNet(ctx, &pb.CreateCLADNetRequest{
+		CladnetSpecification: &pb.CLADNetSpecification{
+			Id:               "",
+			Name:             "CLADNet01",
+			Ipv4AddressSpace: "192.168.77.0/26",
+			Description:      "Alvin's CLADNet01"}})
+
+	if err != nil {
+		log.Fatalf("could not request: %v", err)
+	}
+
+	log.Printf("Config: %v", r)
+}
