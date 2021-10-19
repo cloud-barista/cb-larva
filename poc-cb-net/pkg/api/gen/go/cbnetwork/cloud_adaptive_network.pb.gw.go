affb000858c0c99bc54661d7a00c2c46ac4f8dd4
Yunkon (Alvin) Kim <hermitkim1@gmail.com>
2021-10-19 17:12:27 +0900

    Add `update` and `delete` API skeletons to the proto
    - Add deleteCLADNet() rpc
    - Add DeletionResult message
    - Add updateCLADNet() rpc
    - Regenerate ProtoBuffers for Golang

--- poc-cb-net/pkg/api/gen/go/cbnetwork/cloud_adaptive_network.pb.gw.go
+++ poc-cb-net/pkg/api/gen/go/cbnetwork/cloud_adaptive_network.pb.gw.go
@@ -190,6 +190,76 @@ func local_request_CloudAdaptiveNetwork_RecommendAvailableIPv4PrivateAddressSpac
 
 }
 
+var (
+	filter_CloudAdaptiveNetwork_DeleteCLADNet_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_CloudAdaptiveNetwork_DeleteCLADNet_0(ctx context.Context, marshaler runtime.Marshaler, client CloudAdaptiveNetworkClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq CLADNetID
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_CloudAdaptiveNetwork_DeleteCLADNet_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.DeleteCLADNet(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_CloudAdaptiveNetwork_DeleteCLADNet_0(ctx context.Context, marshaler runtime.Marshaler, server CloudAdaptiveNetworkServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq CLADNetID
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_CloudAdaptiveNetwork_DeleteCLADNet_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.DeleteCLADNet(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
+func request_CloudAdaptiveNetwork_UpdateCLADNet_0(ctx context.Context, marshaler runtime.Marshaler, client CloudAdaptiveNetworkClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq CLADNetSpecification
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.UpdateCLADNet(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_CloudAdaptiveNetwork_UpdateCLADNet_0(ctx context.Context, marshaler runtime.Marshaler, server CloudAdaptiveNetworkServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq CLADNetSpecification
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.UpdateCLADNet(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterCloudAdaptiveNetworkHandlerServer registers the http handlers for service CloudAdaptiveNetwork to "mux".
 // UnaryRPC     :call CloudAdaptiveNetworkServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -308,6 +378,52 @@ func RegisterCloudAdaptiveNetworkHandlerServer(ctx context.Context, mux *runtime
 		}
 
 		forward_CloudAdaptiveNetwork_RecommendAvailableIPv4PrivateAddressSpaces_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("DELETE", pattern_CloudAdaptiveNetwork_DeleteCLADNet_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/cbnet.CloudAdaptiveNetwork/DeleteCLADNet", runtime.WithHTTPPathPattern("/v1/cladnet"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_CloudAdaptiveNetwork_DeleteCLADNet_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_CloudAdaptiveNetwork_DeleteCLADNet_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("PUT", pattern_CloudAdaptiveNetwork_UpdateCLADNet_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/cbnet.CloudAdaptiveNetwork/UpdateCLADNet", runtime.WithHTTPPathPattern("/v1/cladnet"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_CloudAdaptiveNetwork_UpdateCLADNet_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_CloudAdaptiveNetwork_UpdateCLADNet_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -452,6 +568,46 @@ func RegisterCloudAdaptiveNetworkHandlerClient(ctx context.Context, mux *runtime
 
 	})
 
+	mux.Handle("DELETE", pattern_CloudAdaptiveNetwork_DeleteCLADNet_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/cbnet.CloudAdaptiveNetwork/DeleteCLADNet", runtime.WithHTTPPathPattern("/v1/cladnet"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_CloudAdaptiveNetwork_DeleteCLADNet_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_CloudAdaptiveNetwork_DeleteCLADNet_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("PUT", pattern_CloudAdaptiveNetwork_UpdateCLADNet_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/cbnet.CloudAdaptiveNetwork/UpdateCLADNet", runtime.WithHTTPPathPattern("/v1/cladnet"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_CloudAdaptiveNetwork_UpdateCLADNet_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_CloudAdaptiveNetwork_UpdateCLADNet_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -465,6 +621,10 @@ var (
 	pattern_CloudAdaptiveNetwork_CreateCLADNet_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "cladnet"}, ""))
 
 	pattern_CloudAdaptiveNetwork_RecommendAvailableIPv4PrivateAddressSpaces_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "cladnet", "available-ipv4-address-spaces"}, ""))
+
+	pattern_CloudAdaptiveNetwork_DeleteCLADNet_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "cladnet"}, ""))
+
+	pattern_CloudAdaptiveNetwork_UpdateCLADNet_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "cladnet"}, ""))
 )
 
 var (
@@ -477,4 +637,8 @@ var (
 	forward_CloudAdaptiveNetwork_CreateCLADNet_0 = runtime.ForwardResponseMessage
 
 	forward_CloudAdaptiveNetwork_RecommendAvailableIPv4PrivateAddressSpaces_0 = runtime.ForwardResponseMessage
+
+	forward_CloudAdaptiveNetwork_DeleteCLADNet_0 = runtime.ForwardResponseMessage
+
+	forward_CloudAdaptiveNetwork_UpdateCLADNet_0 = runtime.ForwardResponseMessage
 )
